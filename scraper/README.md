44b04330ea9c00668b93c45611102534bf53221d
Edikan Inyang <edikaninyang24@gmail.com>
2025-12-23 23:36:59 +0100

    Add scraper folder structure with Puppeteer/Cheerio starter

---
+++ scraper/README.md
@@ -0,0 +1,18 @@
+\# NexusCart Scraper
+
+
+
+This folder contains scraper logic for NexusCart.
+
+
+
+\## Installation
+
+
+
+```bash
+
+npm install
+
+
+
---
+++ scraper/src/index.js
@@ -0,0 +1,5 @@
+console.log("NexusCart Scraper Running 🚀");
+
+// Example: Later you can import retailer scrapers
+// const amazonScraper = require("./retailers/amazon");
+// amazonScraper.run();
